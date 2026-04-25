e6ba4b2544b87c76d1bef3b2b2aecda6c598c543
Arpan25AK <akarpan2005@gmail.com>
2026-04-25 15:18:44 +0530

    created and updated config files

--- src/main/java/com/assignment/Core_Api_Guardrails/config/AppConfig.java
+++ src/main/java/com/assignment/Core_Api_Guardrails/config/AppConfig.java
@@ -1,4 +1,20 @@
 package com.assignment.Core_Api_Guardrails.config;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@Getter
 public class AppConfig {
+    @Value("${app.redis.bot-cooldown-ttl}")
+    private long botCooldownTtl;
+
+    @Value("${app.redis.notif-cooldown-ttl}")
+    private long notifCooldownTtl;
+
+    @Value("${app.redis.horizontal-cap}")
+    private long horizontalCap;
+
+    @Value("${app.redis.vertical-cap}")
+    private int verticalCap;
 }
